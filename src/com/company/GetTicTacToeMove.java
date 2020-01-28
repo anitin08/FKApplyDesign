b38cc67a5b4bbb0e16269fa6c4532e71d3a06d31
nitin <nitin.agrawal@ET-C02YQEGRLVCF.local>
2020-01-28 17:20:43 +0530

    Revert " Refactoring code"
    This reverts commit d958261efd4dcfa380122247e04057af3339d022.
    
     Reverting changes

---
+++ src/com/company/GetTicTacToeMove.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class GetTicTacToeMove {
+
+        int x;
+        int y;
+        char c;
+        public GetTicTacToeMove(int x, int y, char  c)
+        {
+            this.x=x;
+            this.y=y;
+            this.c=c;
+        }
+        public GetTicTacToeMove()
+        {}
+
+    }
+
+
