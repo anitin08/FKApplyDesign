c48063091b6a904d3040f572b7571b3f46a0522b
nitin <nitin.agrawal@ET-C02YQEGRLVCF.local>
2020-01-29 11:34:05 +0530

    Adding changes in GetStartFunction() to add feature no 7

--- src/com/company/Game_Mode_Human_Human.java
+++ src/com/company/Game_Mode_Human_Human.java
@@ -7,29 +7,49 @@ public class Game_Mode_Human_Human extends GameMode {
     Scanner input = new Scanner(System.in);
     private int row,col;
 
-    public void StartGame() {
-        for (int i = 0; i < board.tictactoeboard.length; i++) {
-            for (int j = 0; j < board.tictactoeboard[0].length; j++) {
-                board.tictactoeboard[i][j] = '_';
+
+    public void Start()
+    {
+        for(int i=0;i<board.tictactoeboard.length;i++)
+        {
+            for(int j=0;j<board.tictactoeboard[0].length;j++) {
+                System.out.println("Game in Cell At position" +i+" "+j);
+                board.winner[i][j] = StartGame(board.tictactoeboard[i][j].cell);
+                //System.out.println("winner of this match is"+board.winner[i][j]);
+            }
+            if(!winner(board.winner))
+                break;
+
+
+        }
+        char c =finalWinner(board.winner);
+
+        System.out.println("winner is "+c);
+    }
+    public char StartGame(char cell[][]) {
+
+        for (int i = 0; i < cell.length; i++) {
+            for (int j = 0; j < cell[0].length; j++) {
+                cell[i][j] = '_';
             }
         }
 
 
         board.printboard();
 
-        while (winner()) {
-            if(isFull()==false) {
+        while (winner(cell)) {
+            if(isFull(cell)==false) {
                 row = input.nextInt();
                 col = input.nextInt();
                 if ((row == 0 || col == 0) || (row > 3 || col > 3)) {
                     System.out.println("You've inputed place, which is out of the board!\nTry again!");
 
                 } else {
-                    if (FullPlace(row, col)) {
+                    if (FullPlace(cell,row, col)) {
                         System.err.println("The place is taken");
 
                     } else {
-                        board.tictactoeboard[row - 1][col - 1] = playerChanger.getTurn();
+                        cell[row - 1][col - 1] = playerChanger.getTurn();
                         board.printboard();
                         playerChanger.whichPlayer();
                     }
@@ -40,14 +60,12 @@ public class Game_Mode_Human_Human extends GameMode {
             else
                 break;
         }
-            if(winner()==false){
-                playerChanger.whichPlayer();
-            System.out.println("the winner is " + playerChanger.getTurn());
-            }
-            else
-            {
-              System.out.println("No one is winner");
-            }
+        if(winner(cell)==false){
+            playerChanger.whichPlayer();
+            // System.out.println("the winner is " + playerChanger.getTurn());
+            return playerChanger.getTurn();
+        }
+        return 'N';
 
     }
 }
