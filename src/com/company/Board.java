b2ce149c2eeb4863d04b78cdc920a904b47357a6
nitin <nitin.agrawal@ET-C02YQEGRLVCF.local>
2020-01-29 11:30:50 +0530

    Refactoring the code for the requirements of phase 2

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,19 +1,38 @@
 package com.company;
 
 public class Board {
-   static char tictactoeboard[][];
-   Board(int n)
-   {
-       tictactoeboard = new char[n][n];
-   }
-    public void printboard() {
+    static  Cell tictactoeboard[][];
+    static char winner[][];
+    Board(int n)
+    {
+        tictactoeboard = new Cell[n][n];
+        winner = new char[n][n];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                tictactoeboard[i][j]= new Cell();
+
+    }
+    public void printboard()
+    {
+        for(int i=0;i<tictactoeboard.length;i++)
+        {
+            for(int j=0;j<tictactoeboard[0].length;j++)
+            {
+                print(tictactoeboard[i][j].cell);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void print(char cell[][]) {
 
         System.out.println("-------------");
-        for (int i = 0; i <tictactoeboard.length ; i++) {
+        for (int i = 0; i <3 ; i++) {
             System.out.print("| ");
-            for (int j = 0; j <tictactoeboard[0].length ; j++) {
+            for (int j = 0; j <3 ; j++) {
 
-                System.out.print(tictactoeboard[i][j] + " | ");
+                System.out.print(cell[i][j] + " | ");
 
             }
             System.out.println();
