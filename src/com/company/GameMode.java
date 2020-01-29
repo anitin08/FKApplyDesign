0a26013c9bbc9fff277af7e5db89c2a7d65a04fc
nitin <nitin.agrawal@ET-C02YQEGRLVCF.local>
2020-01-29 11:31:53 +0530

    Adding feature 7 :  Tic tac toe on 9*9 board

--- src/com/company/GameMode.java
+++ src/com/company/GameMode.java
@@ -1,82 +1,267 @@
 package com.company;
+import java.util.Scanner;
 
 public abstract class GameMode {
 
 
-    Board board =  new Board(3);
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    Board board =  new Board(n);
 
 
-    ///function for checking winner
+
     public boolean checkSame(char a,char b,char c)
     {
-      if((a!='_')&&(a==b)&&(a==c))
-         return true;
+        if((a!='N')&&(a!='\u0000')&&(a!='_')&&(a==b)&&(a==c))
+            return true;
 
-     return false;
-    }
-    public boolean winner()
-    {
-    for(int i=0;i<board.tictactoeboard.length;i++)
-    {
-       if( checkSame(board.tictactoeboard[i][0],board.tictactoeboard[i][1],board.tictactoeboard[i][2]))
-          return false;
-
+        return false;
     }
 
-        for(int i=0;i<board.tictactoeboard.length;i++)
+    public boolean winner(char cell[][])
+    {
+        int countx=0;
+        int counto=0;
+
+        for(int i=0;i<cell.length;i++)
         {
-            if( checkSame(board.tictactoeboard[0][i],board.tictactoeboard[1][i],board.tictactoeboard[2][i]))
-              return false;
+            for(int j=0;j<cell[0].length;j++)
+            {
+                if(cell[i][j]=='X')
+                    countx++;
+                else if(cell[i][j]=='O')
+                    counto++;
+
+            }
+            if(counto==cell.length||countx==cell.length)
+                return false ;
+            counto=0;
+            countx=0;
+        }
+        counto=0;
+        countx=0;
+
+        for(int j=0;j<cell[0].length;j++)
+        {
+            for(int i=0;i<cell.length;i++)
+            {
+                if(cell[j][i]=='X')
+                    countx++;
+                else if( cell[i][j]=='O')
+                    counto++;
+
+            }
+            if(counto==cell.length||countx==cell.length)
+                return false;
+            counto=0;
+            countx=0;
 
         }
+        //right diagonal
+        counto=0;
+        countx=0;
+        for(int i=0;i<cell.length;i++)
+        {
+            if(cell[i][i]=='X')
+                countx++;
+            else if(cell[i][i]=='O')
+                counto++;
 
-        if(checkSame(board.tictactoeboard[0][0],board.tictactoeboard[1][1],board.tictactoeboard[2][2]))
-          return  false;
+        }
+        if(counto==cell.length||countx==cell.length)
+            return false ;
 
-        if(checkSame(board.tictactoeboard[0][2],board.tictactoeboard[1][1],board.tictactoeboard[2][0]))
-            return false;
 
-            return true;
+
+        //left diagonal
+        counto=0;
+        countx=0;
+
+        for(int i=0;i<cell.length;i++)
+        {
+
+            if(cell[i][cell[0].length-i-1]=='X')
+                countx++;
+            else if(cell[i][cell[0].length-i-1]=='O')
+                counto++;
+
+        }
+        if(counto==cell.length|| countx==cell.length)
+            return false ;
+
+
+
+
+        return true;
+
+//       for(int i=0;i<cell.length;i++)
+//    {
+//       if( checkSame(cell[i][0],cell[i][1],cell[i][2]))
+//              return false;
+//
+//    }
+//
+//        for(int i=0;i<cell[0].length;i++)
+//        {
+//            if( checkSame(cell[0][i],cell[1][i],cell[2][i]))
+//              return false;
+//
+//        }
+//
+//        if(checkSame(cell[0][0],cell[1][1],cell[2][2]))
+//            return  false;
+//
+//        if(checkSame(cell[0][2],cell[1][1],cell[2][0]))
+//            return false;
+//
+//            return true;
+    }
+
+    public char finalWinner(char cell[][])
+    {
+        int countx=0;
+        int counto=0;
+
+        for(int i=0;i<cell.length;i++)
+        {
+            for(int j=0;j<cell[0].length;j++)
+            {
+                if(cell[i][j]=='X')
+                    countx++;
+                else if(cell[i][j]=='O')
+                    counto++;
+
+            }
+            if(counto==cell.length)
+                return 'O';
+            else if(countx==cell.length)
+                return 'X';
+            counto=0;
+            countx=0;
+        }
+        counto=0;
+        countx=0;
+
+        for(int j=0;j<cell[0].length;j++)
+        {
+            for(int i=0;i<cell.length;i++)
+            {
+                if(cell[j][i]=='X')
+                    countx++;
+                else if( cell[i][j]=='O')
+                    counto++;
+
+            }
+            if(counto==cell.length)
+                return 'O';
+            else if(countx==cell.length)
+                return 'X';
+
+            counto=0;
+            countx=0;
+        }
+        //right diagonal
+        counto=0;
+        countx=0;
+        for(int i=0;i<cell.length;i++)
+        {
+            if(cell[i][i]=='X')
+                countx++;
+            else if(cell[i][i]=='O')
+                counto++;
+
+        }
+        if(counto==cell.length)
+            return 'O';
+        else if (countx==cell.length)
+            return 'X' ;
+
+
+
+        //left diagonal
+        counto=0;
+        countx=0;
+
+        for(int i=0;i<cell.length;i++)
+        {
+
+            if(cell[i][cell[0].length-i-1]=='X')
+                countx++;
+            else if(cell[i][cell[0].length-i-1]=='O')
+                counto++;
+
+        }
+        if(counto==cell.length)
+            return 'O';
+
+        else if( countx==cell.length)
+            return 'X';
+
+
+
+
+        return 'N';
+//        for(int i=0;i<cell.length;i++)
+//        {
+//            if( checkSame(cell[i][0],cell[i][1],cell[i][2]))
+//                return cell[i][0];
+//
+//        }
+//
+//        for(int i=0;i<cell[0].length;i++)
+//        {
+//            if( checkSame(cell[0][i],cell[1][i],cell[2][i]))
+//                return cell[0][i];
+//
+//        }
+//
+//        if(checkSame(cell[0][0],cell[1][1],cell[2][2]))
+//            return  cell[0][0];
+//
+//        if(checkSame(cell[0][2],cell[1][1],cell[2][0]))
+//            return cell[0][0];
+//
+//        return 'N';
     }
 
 
 
 
 
-    public boolean isFull()
+    public boolean isFull(char cell[][])
     {
-      int n=0;
-     for(int i =0;i<board.tictactoeboard.length;i++)
-     {
-      for(int j=0;j<board.tictactoeboard[0].length;j++)
-      {
-          if(board.tictactoeboard[i][j]=='X'||board.tictactoeboard[i][j]=='O')
-          {
-              n++;
-          }
-      }
+        int n=0;
+        for(int i =0;i<cell.length;i++)
+        {
+            for(int j=0;j<cell[0].length;j++)
+            {
+                if(cell[i][j]=='X'||cell[i][j]=='O')
+                {
+                    n++;
+                }
+            }
 
-     }
-       if(n==(board.tictactoeboard.length*board.tictactoeboard[0].length))
+        }
+        if(n==(cell.length*cell[0].length))
             return true;
 
-       return false;
+        return false;
     }
 
     ///function for checking cell is already full
-    public boolean FullPlace(int row ,int col) {
+    public boolean FullPlace(char cell[][],int row ,int col) {
         {
 
-            if (board.tictactoeboard[row - 1][col - 1] == 'X' ||
-                    board.tictactoeboard[row - 1][col - 1] == 'O') {
+            if (cell[row - 1][col - 1] == 'X' ||
+                    cell[row - 1][col - 1] == 'O') {
                 return true;
             }
             return false;
 
         }
     }
-      //function for starting a game
+    //function for starting a game
 
-       public abstract void StartGame ();
-    }
+    public abstract char StartGame (char cell[][]);
+}
 
