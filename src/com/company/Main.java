d2904edb899d583e078eb727a97408ab8111b486
nitin <nitin.agrawal@ET-C02YQEGRLVCF.local>
2020-01-29 11:33:01 +0530

    Adding cell input from user to know about the board size

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,37 @@
 package com.company;
 
 import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
+        System.out.println("enter the Game mode ");
+        System.out.println("press1:human to human");
+        System.out.println("press2:human to computer");
+        int input = sc.nextInt();
+        // System.out.println((input==1));
+
+        while(!((input==1)||(input==2)))
         {
-            Scanner sc  = new Scanner(System.in);
-
-            System.out.println("enter the Game mode ");
-            System.out.println("press1:human to human");
-            System.out.println("press2:human to computer");
-            int input = sc.nextInt();
-            // System.out.println((input==1));
-
-            while(!((input==1)||(input==2)))
-            {
-                System.out.println("invalid input please enter a valid input");
-                input = sc.nextInt();
-
-            }
-            if(input==1)
-            {
-                Game_Mode_Human_Human allGame = new Game_Mode_Human_Human();
-                allGame.StartGame();
-            }
-            else if(input==2)
-            {
-                Game_Mode_Human_Computer humantocomputer= new Game_Mode_Human_Computer();
-                humantocomputer.StartGame();
-            }
-
-
-
-
+            System.out.println("invalid input please enter a valid input");
+            input = sc.nextInt();
 
         }
+        if(input==1)
+        {    System.out.println("Enter the of cells you want play");
+            Game_Mode_Human_Human allGame = new Game_Mode_Human_Human();
+
+            allGame.Start();
+        }
+        else if(input==2)
+        {   System.out.println("Enter the of cells you want play");
+            Game_Mode_Human_Computer humantocomputer= new Game_Mode_Human_Computer();
+            humantocomputer.Start();
+        }
+
+
+    }
 }
