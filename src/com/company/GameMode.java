778204fe2c4e0033b5b0b371bc3973518fae93d0
anitin08 <anitin08@gmail.com>
2020-01-31 14:06:44 +0530

    Removing unnecessary modules

--- src/com/company/GameMode.java
+++ src/com/company/GameMode.java
@@ -114,116 +114,85 @@ public abstract class GameMode {
 //        if(checkSame(cell[0][2],cell[1][1],cell[2][0]))
 //            return false;
 //
-//            return true;
-    }
+//            return tr
+}
 
-    public char finalWinner(char cell[][])
-    {
-        int countx=0;
-        int counto=0;
+    public char finalWinner(char cell[][]) {
+        int countx = 0;
+        int counto = 0;
 
-        for(int i=0;i<cell.length;i++)
-        {
-            for(int j=0;j<cell[0].length;j++)
-            {
-                if(cell[i][j]=='X')
+        for (int i = 0; i < cell.length; i++) {
+            for (int j = 0; j < cell[0].length; j++) {
+                if (cell[i][j] == 'X')
                     countx++;
-                else if(cell[i][j]=='O')
+                else if (cell[i][j] == 'O')
                     counto++;
 
             }
-            if(counto==cell.length)
+            if (counto == cell.length)
                 return 'O';
-            else if(countx==cell.length)
+            else if (countx == cell.length)
                 return 'X';
-            counto=0;
-            countx=0;
+            counto = 0;
+            countx = 0;
         }
-        counto=0;
-        countx=0;
+        counto = 0;
+        countx = 0;
 
-        for(int j=0;j<cell[0].length;j++)
-        {
-            for(int i=0;i<cell.length;i++)
-            {
-                if(cell[j][i]=='X')
+        for (int j = 0; j < cell[0].length; j++) {
+            for (int i = 0; i < cell.length; i++) {
+                if (cell[j][i] == 'X')
                     countx++;
-                else if( cell[i][j]=='O')
+                else if (cell[i][j] == 'O')
                     counto++;
 
             }
-            if(counto==cell.length)
+            if (counto == cell.length)
                 return 'O';
-            else if(countx==cell.length)
+            else if (countx == cell.length)
                 return 'X';
 
-            counto=0;
-            countx=0;
+            counto = 0;
+            countx = 0;
         }
         //right diagonal
-        counto=0;
-        countx=0;
-        for(int i=0;i<cell.length;i++)
-        {
-            if(cell[i][i]=='X')
+        counto = 0;
+        countx = 0;
+        for (int i = 0; i < cell.length; i++) {
+            if (cell[i][i] == 'X')
                 countx++;
-            else if(cell[i][i]=='O')
+            else if (cell[i][i] == 'O')
                 counto++;
 
         }
-        if(counto==cell.length)
+        if (counto == cell.length)
             return 'O';
-        else if (countx==cell.length)
-            return 'X' ;
-
-
-
-        //left diagonal
-        counto=0;
-        countx=0;
-
-        for(int i=0;i<cell.length;i++)
-        {
-
-            if(cell[i][cell[0].length-i-1]=='X')
-                countx++;
-            else if(cell[i][cell[0].length-i-1]=='O')
-                counto++;
-
-        }
-        if(counto==cell.length)
-            return 'O';
-
-        else if( countx==cell.length)
+        else if (countx == cell.length)
             return 'X';
 
 
+        //left diagonal
+        counto = 0;
+        countx = 0;
+
+        for (int i = 0; i < cell.length; i++) {
+
+            if (cell[i][cell[0].length - i - 1] == 'X')
+                countx++;
+            else if (cell[i][cell[0].length - i - 1] == 'O')
+                counto++;
+
+        }
+        if (counto == cell.length)
+            return 'O';
+
+        else if (countx == cell.length)
+            return 'X';
 
 
         return 'N';
-//        for(int i=0;i<cell.length;i++)
-//        {
-//            if( checkSame(cell[i][0],cell[i][1],cell[i][2]))
-//                return cell[i][0];
-//
-//        }
-//
-//        for(int i=0;i<cell[0].length;i++)
-//        {
-//            if( checkSame(cell[0][i],cell[1][i],cell[2][i]))
-//                return cell[0][i];
-//
-//        }
-//
-//        if(checkSame(cell[0][0],cell[1][1],cell[2][2]))
-//            return  cell[0][0];
-//
-//        if(checkSame(cell[0][2],cell[1][1],cell[2][0]))
-//            return cell[0][0];
-//
-//        return 'N';
-    }
 
+    }
 
 
 
